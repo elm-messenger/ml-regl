dd001c144e0e75ac5774926c0826483537a48bee
Yiming Xiang <yiming@cs.utexas.edu>
2025-09-12 16:59:44 -0500

    feat: regl

--- test/test_ml_regl.ml
+++ test/test_ml_regl.ml
@@ -1,4 +1,16 @@
 open Ml_regl
+open Js_of_ocaml
 
 let mycircle = Regl_builtin_programs.circle (3., 4.) 5. Color.red
-let _ = Regl.init_regl ()
+let canvas = Regl.init_regl ()
+
+let _ =
+  Js.export "App"
+    (Js.Unsafe.obj
+       [|
+         ( "canvas",
+           Js.Unsafe.inject (fun _ ->
+               match !canvas with
+               | Some c -> c
+               | None -> failwith "No canvas initialized") );
+       |])
